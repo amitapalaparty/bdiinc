658a9934d7597638b763e231d56fb7eb47233e77
Veda Palaparty <veda@Vedas-Air.home>
2023-10-30 19:23:22 -0700

    First commit

---
+++ bdi/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+    SECRET_KEY = 'nZ4jxrNG7U5Y0oan'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'amitapalaparty@gmail.com'
+    MAIL_PASSWORD = 'xnfexgnsepiyfmum'
